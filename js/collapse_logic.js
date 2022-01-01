e2d0fad9e584994f803fea402422f8c7888e2eee
Mandy Meindersma <meinders@ualberta.ca>
2022-01-01 14:48:05 -0700

    fixing the broken menu button

---
+++ js/collapse_logic.js
@@ -0,0 +1,5 @@
+/* so I think the earlier versions of j query had something to do this easier but since I upgraaded the menu broke */
+
+$("button.navbar-toggle").click(function(){
+  $("#bs-example-navbar-collapse-1").toggleClass("collapse");
+});
